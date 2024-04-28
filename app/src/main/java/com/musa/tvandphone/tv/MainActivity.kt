8bd3c2ba000cb5feaa82901dfdbf8d6ab199150f
musa <musagak@gmail.com>
2024-04-28 20:38:10 +0200

    Initial commit

---
+++ app/src/main/java/com/musa/tvandphone/tv/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.musa.tvapptest
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.musa.tvandphone.R
+
+/**
+ * Loads [MainFragment].
+ */
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tv_activity_main)
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_browse_fragment, MainFragment())
+                .commitNow()
+        }
+    }
+}
